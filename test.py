93b31e675c8af06dc8cb00462ab1a214ce2ac6b7
saamon <suzuken2960@gmail.com>
2022-12-01 01:30:39 +0900

    Streamlitとの連携

--- test.py
+++ test.py
@@ -2,23 +2,30 @@ import os
 import openai
 from dotenv import load_dotenv  # この行を追加
 
-load_dotenv()  # この行を追加（環境変数を読み込む）
+import streamlit as st
+st.title('GPT-3 Demo')
 
-openai.api_key = os.getenv("OPENAI_API_KEY")  # この行を追加
+# promptを入力するテキストボックスを作成
+prompt = st.text_area('prompt')
 
-# 単語から文章を生成する
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="ゼミ 遅刻 謝罪",
-    temperature=0.5,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-)
+# ボタンを作成
+if st.button('Generate'):
+    # .envファイルを読み込む
+    load_dotenv()
+    # .envファイルからAPIキーを取得
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    # GPT-3に渡すパラメータを設定
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
 
-# 結果を表示する
-print(response["choices"][0]["text"])
+    # GPT-3の出力を表示
+    st.write(response['choices'][0]['text'])
 
-# 結果
-# ご迷惑をおかけして申し訳ございません。予定より遅れて参加できなかったことをお詫び申し上げます。今後は時間厳守して参加できるよう努力します。
+    # streamlit run test.py（ファイル名）で実行
