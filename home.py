2d4c2709840c6aefb1fb467ad75d5f1cba33bdcd
Kento Suzuki <suzuken2960@gmail.com>
2023-01-10 17:07:40 +0900

    マルチページ対応

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("誰でもビジネスマン")
+
+# Webページの紹介を書く
+st.write("このWebアプリは、GPT-3を使って、あなたの経歴をもとに、ビジネス文章の作成をお手伝いします。")
+
+# 3種類の機能についての説明を書く
+st.write("このWebアプリは、3つの機能があります。")
+
+# streamlit run home.pyで実行
