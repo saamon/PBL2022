59c61818fd5e010c1d11fce1945ebb2cc6b07604
Kento Suzuki <suzuken2960@gmail.com>
2023-01-10 13:05:11 +0900

    入力欄をForm対応

--- self_appeal.py
+++ self_appeal.py
@@ -5,28 +5,28 @@ import streamlit as st
 
 st.title("自己PRジェネレーター")
 
-# 応募する職種を入力するテキストボックスを作成
-job_type = st.text_input("応募する職種を入力してください")
+with st.form("my_form"):
+    # 応募する職種を入力するテキストボックスを作成
+    job_type = st.text_input("応募する職種を入力してください")
 
-# 経験を簡単に入力するテキストボックスを作成
-experience = st.text_area("経験を簡単に入力してください")
+    # 経験を簡単に入力するテキストボックスを作成
+    experience = st.text_area("経験を簡単に入力してください")
 
-# ユーザーが出力する文章の雰囲気を選択できるセレクトボックスを作成
-# 雰囲気はtemperatureで調整する
-mood = ["大胆", "丁寧", "カジュアル"]
-selected_mood = st.selectbox("出力する文章の雰囲気を選択してください", mood)
+    # ユーザーが出力する文章の雰囲気を選択できるセレクトボックスを作成
+    # 雰囲気はtemperatureで調整する
+    mood = ["大胆", "丁寧", "カジュアル"]
+    selected_mood = st.selectbox("出力する文章の雰囲気を選択してください", mood)
 
-# ボタンを作成
-if st.button("Generate"):
-    # .envファイルを読み込む
-    load_dotenv()
-    # .envファイルからAPIキーを取得
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    # GPT-3に渡すパラメータを設定
-    prompt = f"応募する職種: {job_type}\n経験: {experience}\n雰囲気: {selected_mood}+自己PR文を生成してください\n\n自己PR文:\n"
-    # GPT-3に入力を渡して出力を取得
-    # それぞれ出力した文章をコピーできるようにする
-    # もし、大胆と選択されたらtemperatureを0.9に設定
+    # ボタンを作成
+    submit_button = st.form_submit_button("Generate")
+
+    if submit_button:
+        # .envファイルを読み込む
+        load_dotenv()
+        # .envファイルからAPIキーを取得
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        # GPT-3に渡すパラメータを設定
+        prompt = f"応募する職種: {job_type}\n経験: {experience}\n雰囲気: {selected_mood}+自己PR文を生成してください\n\n自己PR文:\n"
     if selected_mood == "大胆":
         response = openai.Completion.create(
             engine="text-davinci-003",
@@ -39,7 +39,6 @@ if st.button("Generate"):
         )
         # 文章を出力する
         st.write(response["choices"][0]["text"])
-
     # もし、丁寧と選択されたらtemperatureを0.1に設定
     elif selected_mood == "丁寧":
         response = openai.Completion.create(
@@ -66,7 +65,6 @@ if st.button("Generate"):
         )
         # 文章を出力
         st.write(response["choices"][0]["text"])
-
 # このコードを実行するには、以下のコマンドを実行してください。
 
-# streamlit run test.py（ファイル名）で実行
+# streamlit run self_appeal.py（ファイル名）で実行
